7c68a82608890678b2ef7b3c94ef0adea1d87260
christian williams <christian.williamsj@gmail.com>
2019-09-17 22:47:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,3 +9,11 @@ class EmailParser
   def initialize(csv_emails)
        @csv_emails = csv_emails
   end 
+  
+    def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
